31e52be4e3150173ed5ce0bef272cacf4fdfa941
Frankzhaopku <syzhao1988@126.com>
2016-09-07 15:49:02 +0800

    bean definition add.

--- app/service/dataService.js
+++ app/service/dataService.js
@@ -1,3 +1,17 @@
 /**
  * Created by hzzhaoshengyu on 2016/9/7.
  */
+var bearcat = require('bearcat');
+var fs = require('fs');
+var logger = require('pomelo-logger').getLogger('san-alarm', 'dataService');
+
+var DataService = function () {
+  this.$id = "dataService";
+  this.loadPath = process.cwd() + '/config/' + bearcat.getApplicationContext().getEnv() + '/';
+  this.dataConfig = require(this.loadPath + 'data.json');
+};
+
+DataService.prototype.init = function () {
+};
+
+module.exports = DataService;
--- app/service/mailService.js
+++ app/service/mailService.js
@@ -1,3 +1,17 @@
 /**
  * Created by hzzhaoshengyu on 2016/9/7.
  */
+var bearcat = require('bearcat');
+var fs = require('fs');
+var logger = require('pomelo-logger').getLogger('san-alarm', 'mailService');
+
+var MailService = function () {
+  this.$id = "mailService";
+  this.loadPath = process.cwd() + '/config/' + bearcat.getApplicationContext().getEnv() + '/';
+  this.dataConfig = require(this.loadPath + 'data.json');
+};
+
+MailService.prototype.init = function () {
+};
+
+module.exports = MailService;
