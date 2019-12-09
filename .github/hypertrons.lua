eeea7847693717730ae31f72cbe528affad24f45
Frank Zhao <syzhao1988@126.com>
2019-12-09 13:58:58 +0800

    Update hypertrons.lua

--- .github/hypertrons.lua
+++ .github/hypertrons.lua
@@ -1,4 +1,18 @@
--- Auto label on issue open
+-- Copyright 2019 Xlab
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
+-- Auto label on issue/pull open/edit
 local autoLabel = function (e)
   if(e.action == 'opened' or e.action == 'edited') then
     local l = {}
@@ -19,3 +33,22 @@ local autoLabel = function (e)
 end
 on('IssueEvent', autoLabel)
 on('PullRequestEvent', autoLabel)
+
+-- Issue reminder
+sched('Issue reminder', '10/* * * * * *', function ()
+  local data = getData()
+  local committers = config['role']['committers']
+  if (#committers == 0) then
+    return
+  end
+  local msg = 'Please reply this issue, '
+  for i= 1, #committers do
+    msg = msg ... '@' ... committers[i] ... ' '
+  end
+  msg = msg ... '.'
+  for i= 1, #data.issues do
+    if (#data.issues[i].comments == 0 && toNow(data.issues[i].createdAt > 24 * 60 * 60 * 1000)) then
+      addIssueComment(issue[i].number, msg)
+    end
+  end
+end)
